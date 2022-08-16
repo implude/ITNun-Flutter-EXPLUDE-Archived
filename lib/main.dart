64bb9d41e3f825643a65eaead430c93c2b290839
seukhwankim <io0809oi@dimigo.hs.kr>
2022-08-16 21:36:35 +0900

    로고 추가

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,25 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  //예아
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: yeah(),
+    );
+  }
+}
+
+class yeah extends StatelessWidget {
+  const yeah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Yeah'),
+      ),
+      body: Center(
+        child: Image.asset("logo.png"),
+      ),
+    );
   }
 }
