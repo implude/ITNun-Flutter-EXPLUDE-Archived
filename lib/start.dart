30fb035cee67db53247fc10cbbe990742d22f068
이동헌 <dongheon0916@dimigo.hs.kr>
2022-08-17 15:22:41 +0900

    refactor: change widget name

--- lib/start.dart
+++ lib/start.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Start extends StatelessWidget {
-  const Start({Key? key}) : super(key: key);
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
