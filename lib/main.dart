b9f0272905be706741230afe62c3b7f0a97be3b3
이동헌 <dongheon0916@dimigo.hs.kr>
2022-08-17 15:23:47 +0900

    feat: add get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:itnun/start.dart';
 
 void main() {
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: StartPage());
+    return GetMaterialApp(
+        initialRoute: "/",
+        getPages: [GetPage(name: "/", page: () => const StartPage())]);
   }
 }
