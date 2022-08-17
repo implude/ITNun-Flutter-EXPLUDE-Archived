711808cd7e848f8eb9f058847fd67db99f58c1dc
이동헌 <dongheon0916@dimigo.hs.kr>
2022-08-17 15:18:37 +0900

    style: separate file, add const modifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:itnun/start.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,88 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: start(),
-    );
-  }
-}
-class start extends StatelessWidget {
-  const start({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 200,),
-            Container(
-              child: Column(
-                children: [
-                  Text("사람과 정책을 잇는.",style: TextStyle(
-                  fontSize:16,
-                    fontWeight: FontWeight.bold,
-                  )
-                  ),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/images/logo_outlined.png"),
-                        SizedBox(width: 10,),
-                        Text("잇는",style: TextStyle(fontSize: 44,fontWeight: FontWeight.bold),)
-
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-           SizedBox(
-             height: 200,
-           ),
-           SizedBox(
-             height: 60,
-             width: 350,
-             child: OutlinedButton(
-               style: OutlinedButton.styleFrom(
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.all(Radius.circular(20)),
-
-                 ),
-
-                 side: BorderSide(
-                   width: 2.0,
-                   color: Color(0xff3C65F8),
-                 )
-
-               ),
-                 onPressed: (){},
-                 child: Text("로그인",style: TextStyle(fontSize: 20,color: Color(0xff3C65F8),fontWeight: FontWeight.bold),)),
-           ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 60,
-              width: 350,
-              child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-
-                      ),
-
-                    backgroundColor: Color(0xff3C65F8),
-
-                  ),
-                  onPressed: (){},
-                  child: Text("회원가입",style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold),)),
-            ),
-            
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      home: Start(),
     );
   }
 }
