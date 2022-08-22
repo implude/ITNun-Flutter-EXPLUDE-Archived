7d12d9fe77e9fc8bbc0014ea12903673e1330838
이동헌 <dongheon0916@dimigo.hs.kr>
2022-08-22 11:12:23 +0900

    style: code formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:itnun/start.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StartPage(),
+    return GetMaterialApp(
+      initialRoute: "/",
+      getPages: [GetPage(name: "/", page: () => const StartPage())],
     );
   }
 }
