8398da73e1db11fa6637882bfa7fe03ca21832e2
이동헌 <dongheon0916@gmail.com>
2022-09-10 23:21:47 +0900

    feat: Add Kind Search Result Page

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ import 'package:itnun/screens/search/hire/hire_search.dart';
 import 'package:itnun/screens/search/hire/hire_search_detail.dart';
 import 'package:itnun/screens/search/hire/hire_search_result.dart';
 import 'package:itnun/screens/search/kind/kind_search.dart';
+import 'package:itnun/screens/search/kind/kind_search_result.dart';
 import 'package:itnun/screens/search/space/space_search.dart';
 import 'package:itnun/screens/search/space/space_search_detail.dart';
 import 'package:itnun/screens/search/space/space_search_result.dart';
@@ -159,7 +160,9 @@ class MyApp extends StatelessWidget {
         GetPage(
             name: "/search/space/detail",
             page: () => const SpaceSearchDetail()),
-        GetPage(name: "/search/kind", page: () => const KindSearch())
+        GetPage(name: "/search/kind", page: () => const KindSearch()),
+        GetPage(
+            name: "/search/kind/result", page: () => const KindSearchResult())
       ],
     );
   }
