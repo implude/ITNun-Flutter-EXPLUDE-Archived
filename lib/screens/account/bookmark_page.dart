003af26ff8a89c16ee96f6ccd26542f5b6b018d0
MercuryLee <sooseong1230@gmail.com>
2022-10-19 21:03:49 +0900

    bookmark_edit_page, bookmark_page frame

--- lib/screens/account/bookmark_page.dart
+++ lib/screens/account/bookmark_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:itnun/models/policy_info.dart';
 import 'package:itnun/widgets/app_widgets.dart';
 import 'package:itnun/widgets/appbar_widgets.dart';
 import 'package:itnun/widgets/title_subject_widget.dart';
@@ -27,27 +28,22 @@ class BookmarkPage extends StatelessWidget {
           children: [
             const TitleWidget(title: "북마크된 페이지"),
             ...[
-              // PolicyBookmarkBox(
-              //   onMarked: (value) {},
-              //   title: "LH 희망상가",
-              //   category: "창업지원",
-              //   region: "전국",
-              //   marked: true,
-              // ),
-              // PolicyBookmarkBox(
-              //   onMarked: (value) {},
-              //   title: "청춘남녀만남지원",
-              //   category: "생활복지",
-              //   region: "경북",
-              //   marked: true,
-              // ),
-              // KindBookmarkBox(
-              //     title: "체스보드게임",
-              //     category: "기타서비스업",
-              //     region: "부산광역시",
-              //     marked: true,
-              //     onMarked: (value) {}
-              // ),
+              PolicyBookmarkBox(
+                onMarked: (value) {},
+                marked: true,
+                policyInfo: PolicyInfo("", "", "", "", "", "", "", "", "", "", "", "", "", "", ""),
+              ),
+              PolicyBookmarkBox(
+                onMarked: (value) {},
+                marked: true,
+                policyInfo: PolicyInfo("", "", "", "", "", "", "", "", "", "", "", "", "", "", ""),
+              ),
+              KindBookmarkBox(
+                  title: "체스보드게임",
+                  region: "부산광역시",
+                  marked: true,
+                  onMarked: (value) {}
+              ),
             ].map((e) => Padding(
                   padding: EdgeInsets.only(
                       bottom: context.heightTransformer(dividedBy: 60)),
